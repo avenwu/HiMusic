c947795cd7840f6f4d74b78bf0bb426e6cb762df
Chaobin Wu <chaobinwu@tagged.com>
2013-11-16 00:39:51 +0800

    [Home]1.init HiMusic project;2.add empty search view

---
+++ HiMusic/src/main/java/com/avenwu/himusic/UIHelper.java
@@ -0,0 +1,18 @@
+package com.avenwu.himusic;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * @author chaobin
+ * @date 11/15/13.
+ */
+public class UIHelper {
+    public static void toast(Context context, int id) {
+        Toast.makeText(context, id, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void toast(Context context, CharSequence content) {
+        Toast.makeText(context, content, Toast.LENGTH_SHORT).show();
+    }
+}
